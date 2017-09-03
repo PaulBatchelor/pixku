4b26e9c803696947d2bda691ecfa8e5805be0f13
Paul Batchelor <thisispaulbatchelor@gmail.com>
2017-09-02 22:05:56 -0700

    improved shell script for installing libraries

--- _libs/run.sh
+++ _libs/run.sh
@@ -1,9 +1,18 @@
+if [ "$#" -eq 0 ]
+then
+echo "Usage: sh run.sh [install|clean]"
+exit
+fi
+
 RUNT_LIBS=(
 cray 
 runt-cairo 
 runt-img 
 runt-plumber
 runt-scheme)
+
+install()
+{
 
 for val in ${RUNT_LIBS[@]}
 do
@@ -12,3 +21,24 @@ do
     make install
     cd -
 done
+}
+
+clean()
+{
+
+for val in ${RUNT_LIBS[@]}
+do
+    cd $val
+    make 
+    make clean 
+    cd -
+done
+}
+
+if [ "$1" == install ] 
+then
+install
+elif [ "$1" == "clean" ]
+then
+clean
+fi
